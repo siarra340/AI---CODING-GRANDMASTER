4ad92623c542d8c14fa7143d25def6b738c92746
siarra340 <siarra340@gmail.com>
2026-07-25 11:21:16 +0600

    Activity 1: Print Statements and Comments
    (Module 7: Lesson Intro to Python and Setting up Dev Tools| Activity 1: Print Statements and Comments)

---
+++ activity_1.py
@@ -0,0 +1,11 @@
+# printing Hello World
+print("hello world!")
+
+# printing my name
+print("Siarra")
+
+# printing fun fact
+print("My favourite color is blue")
+
+# printing addition
+print("Total of two numbers is", 3+7)
