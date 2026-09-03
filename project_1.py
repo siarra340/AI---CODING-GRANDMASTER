a120d13cf0ea0829755209d729bdff298938f578
siarra340 <siarra340@gmail.com>
2026-09-04 00:14:33 +0600

    Project: My Library Book Organizer
    (Module 7: Lesson: Data Structures in Python- 1| Project: My Library Book Organizer)

--- project_1.py
+++ project_1.py
@@ -1,19 +1,51 @@
-def create_fibonacci(n):
-    series = []
-    num1, num2 = 0,1
+books = ["Harry Potter", "Matilda", "The Jungle Book", "Charlotte's Web", "Wonder"]
 
-    for _ in range(n):
-        series.append(num1)
-        num1, num2 = num2, num1 + num2
+print("Library Book List:", books)
+print("Total Books:", len(books))
+print(f"First Book: {books[0]}")
+print(f"Last Book: {books[-1]}")
+print("First Two Books:", books[:2])
 
-    return series
+books.append("Diary of a Wimpy Kid")
+print("After Adding Book:", books)
 
-term = int(input("Enter the number of terms: "))
+books.remove("The Jungle Book")
+print("After Removing a Book:", books)
 
-if term <= 0:
-    print("Enter a postive integer")
-else:
-    ans = create_fibonacci(term)
+books.sort()
+print(f"Books Sorted Alphabetically: {books}")
 
-    print(f"The Fibonacci sequence up to {term} terms: ")
-    print(ans)
+books.reverse()
+print(f"Books in Reverse Order: {books}")
+
+librarian = {"name": "Ms. Priya", "section": "Children's Books", "experience": 5}
+
+print(f"Librarian profile: {librarian}")
+
+
+print("Librarian Name:", librarian["name"])
+print("Librarian Section:", librarian["section"])
+print("Experience:", librarian["experience"])
+
+librarian["experience"]=  7
+print("Updated Experience:", librarian)
+
+librarian["email"]= "priya@schoollibrary.com"
+print("After Adding Email:", librarian)
+
+librarian.pop("section")
+print("After Removing Section:", librarian)
+
+books_ids = [101, 102, 103, 104, 105]
+book_names = ["Matilda", "Wonder", "Harry Potter", "Charlotte's Web", "Diary of a Wimpy Kid"]
+
+book_directory = dict(zip(books_ids, book_names))
+print("Book Directory:", book_directory)
+
+print("============================")
+print("LIBRARY ORGANISER SUMMARY")
+print("============================")
+print("Available Books: ", books)
+print("Librarian Details:", librarian)
+print("Book ID Directory:", book_directory)
+print("============================")
