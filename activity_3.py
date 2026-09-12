79550a3116dd73d7b35376fa362fc0190b89482a
siarra340 <siarra340@gmail.com>
2026-09-12 13:50:03 +0600

    Activity 3: Parrot Bird
    (Module 7: Lesson: Object-Oriented Programming- 1| Activity 3: Parrot Bird)

--- activity_3.py
+++ activity_3.py
@@ -1,8 +1,16 @@
-total_sum = 0
-num = 1
+class Parrot:
 
-while num <=10:
-    total_sum +=num
-    num +=1
+    species = "bird"
 
-print(f"The sum of the first ten natrual numbers is {total_sum}")
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+blu = Parrot("Blu", 10)
+woo = Parrot("Woo", 15)
+
+print("Blu is a {}".format(blu.species))
+print("Woo is also a {}".format(woo.species))
+
+print("{} is {} years old".format(blu.name, blu.age))
+print("{} is {} years old".format(woo.name, woo.age))
