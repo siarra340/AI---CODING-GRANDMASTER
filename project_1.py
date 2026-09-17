7a1d4d2227273c79ea7c68f3c7c76ea64d1e87e7
siarra340 <siarra340@gmail.com>
2026-09-17 23:39:42 +0600

    Project: My Art Gallery Collection Manager
    (Module 7: Lesson: Object Oriented Programming- 2| Project: My Art Gallery Collection Manager)

--- project_1.py
+++ project_1.py
@@ -1,21 +1,64 @@
-class Robot:
+class ArtGallery:
 
-    function = "assit humans and make tasks easier"
+    def __init__(self, gallery_name, location):
+        self.gallery_name = gallery_name
+        self.location = location
+        self.artworks = []
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        print(f"\nWelcome to {self.gallery_name}")
+        print(f"Location: {self.location}")
+        print("Art gallery collection is ready.")
 
-    def intro(self):
-        print(f"My name is {self.name} and I am {self.age} years old.")
+    def add_artwork(self, artwork):
+        self.artworks.append(artwork)
+        print(f"'{artwork}' has been added to the gallery collection.")
 
-    def hobby(self, hobby):
-        return "{} likes to {} as a hobby".format(self.name, hobby)
+    def remove_artwork(self, artwork):
+        if artwork in self.artworks:
+            self.artworks.remove(artwork)
+            print(f"'{artwork}' has been removed from the gallery collection.")
+        else:
+            print(f"'{artwork}' was not found in the gallery collection.")
 
-robot = Robot("Julie", 10)
+    def display_artworks(self):
+        print(f"\n--- {self.gallery_name} Art Collection ---")
 
-robot.intro()
+        if self.artworks:
+            for i, artwork in enumerate(self.artworks, 1):
+                print(f"{i}. {artwork}")
+        else:
+            print("No artworks have been added yet.")
 
-print(f"{robot.name}'s function is to {robot.function}")
+    def __del__(self):
+        print(f"\nClosing {self.gallery_name}. Thank you for managing the art collection!")
 
-print(robot.hobby("read"))
+gallery = ArtGallery("Creative Canvas Gallery", "Bengaluru")
+
+while True:
+    print("\n=========== ART GALLERY MENU ===========")
+    print("1. Add Artwork")
+    print("2. Remove Artwork")
+    print("3. Display Art Collection")
+    print("4. Exit")
+    print("========================================")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        artwork_name = input("Enter the artwork name to add: ")
+        gallery.add_artwork(artwork_name)
+
+    elif choice == "2":
+        artwork_name = input("Enter the artwork name to remove: ")
+        gallery.remove_artwork(artwork_name)
+
+    elif choice == "3":
+        gallery.display_artworks()
+
+    elif choice == "4":
+        print("Exiting the Art Gallery Collection Manager.")
+
+        del gallery
+        break
+    else:
+        print("Invaild choice. Please enter a number from 1 to 4.")
